5bd6b2f234e5685bf3ffd17815ac6a5d8be49f0f
Victor <victor.vital.souza@alumni.usp.br>
2023-02-01 14:52:06 -0300

    organizando funções

--- extra_functions.R
+++ extra_functions.R
@@ -1,10 +1,16 @@
-
 #Funções
+
 library(tidyverse)
+
 dup_check = function(x) c(duplicated(x, fromLast = TRUE)  | duplicated(x))
+
 check_zero = function(x) ifelse(x == 0 & !is.na(x), TRUE, FALSE)
+
 fix_date <- function(x){as.Date(paste0(x, '01'), '%Y%m%d')}
+
 `%nin%` = Negate(`%in%`)
+
+#Agrupa tudo conglomerados prudenciais a partir dos dados de cadastro
 agrupa_prudencial = function(x){
   
   cadastro <- readRDS('dados/cadastro_instituicao.rds')  %>% mutate(Data = Data %>% fix_date()) %>%
@@ -21,12 +27,16 @@ agrupa_prudencial = function(x){
     summarise_all(sum, na.rm=T) %>% ungroup() %>%
     rename(CodInst=CodConglomeradoPrudencial)
 }
+
+#Rename de modalidades de crédito PF
 rename_modalidade_pf = function(x){
   x %>%
     rename('MOD_PF_CARTAO_CREDITO' = `Cartão de Crédito`, 'MOD_PF_EMPREST_CONSIG_FOLHA' = `Empréstimo com Consignação em Folha`, 
            'MOD_PF_EMPREST_SEM_CONSIG_FOLHA' = `Empréstimo sem Consignação em Folha`, 'MOD_PF_HABITACAO' = `Habitação`, 
            'MOD_PF_OUTROS' = `Outros Créditos`,  'MOD_PF_RURAL_AGROINDUST' = `Rural e Agroindustrial`, 
            'MOD_PF_VEICULOS' = `Veículos`, 'MOD_PF_EXTERIOR' = `Total Exterior Pessoa Física`, 'TOTAL_PF' = `Total da Carteira de Pessoa Física`)}
+
+#Rename de modalidades de crédito PJ
 rename_modalidade_pj <- function(x){
   x %>%
     rename('MOD_PJ_CAPITAL_GIRO' = `Capital de Giro`, 'MOD_PJ_CAPITAL_GIRO_ROTATIVO' =  `Capital de Giro Rotativo`,
@@ -35,6 +45,8 @@ rename_modalidade_pj <- function(x){
            'MOD_PJ_INVESTIMENTO' = `Investimento`, 'MOD_PJ_RECEBIVEIS' = `Operações com Recebíveis`, 
            'MOD_PJ_OUTROS' = `Outros Créditos`, 'MOD_PJ_RURAL_AGROINDUST' = `Rural e Agroindustrial`, 
            'MOD_PJ_EXTERIOR' = `Total Exterior Pessoa Jurídica`, 'TOTAL_PJ' = `Total da Carteira de Pessoa Jurídica`)}
+
+#Rename de Atividade
 rename_atividade <- function(x){
   x %>%
     rename('ATIV_ADMPUB_DEFESA_SEGURIDADE_SOCIAL' = `Administração Pública, Defesa e Seguridade Social`, 'ATIV_PECUARIA_FLORESTAL_PESCA_AQUICULTURA' = `Agricultura, Pecuária, Produção Florestal, Pesca e Aquicultura`, 
@@ -43,6 +55,8 @@ rename_atividade <- function(x){
            'ATIV_OUTROS' = `Outros`,'ATIV_SERV_IND_UTIL_PUB' = `Serviços Industriais de Utilidade Pública`,
            'ATIV_TRANSP_ARMAZEN_CORREIO' = `Transporte, Armazenagem e Correio`, 'ATIV_N_INFO' = `Atividade não Informada ou não se Aplica` ,
            'ATIV_TOTAL_N_INDIV' = `Total não Individualizado Pessoa Jurídica`,'ATIV_TOTAL_EXTERIOR' = `Total Exterior Pessoa Jurídica`, 'TOTAL_PJ' = `Total da Carteira de Pessoa Jurídica`)}
+
+#Rename de fontes de Receita
 rename_receitas <- function(x){
   x %>% rename('RENDA_OPR_CREDITO' = `78203`,
                'RENDA_OPR_LEASING' = `78204`,
@@ -54,6 +68,8 @@ rename_receitas <- function(x){
                'RENDA_TARIFAS' = `78217`,
                'RENDA_PARTICIP' = `78221`,
                'RENDA_OUTROS' = `78222`)}
+
+#Ajuste da DRE para refletir os resultados do trimestre
 ajuste_dre = function(x){
   # 1. De acordo com a Lei nº 4.595, de 31 de dezembro de 1964, as instituições financeiras devem apurar resultados em 30 de junho e em 31 de dezembro de cada ano, obrigatoriamente, 
   # com observância às regras contábeis estabelecidas pelo CMN. Dessa forma, em março e setembro, os dados contábeis referentes às receitas e às despesas correspondem aos saldos 
@@ -65,31 +81,4 @@ ajuste_dre = function(x){
     group_by(Ano,Semestre,TipoInstituicao, CodInst, NomeRelatorio, NumeroRelatorio, Grupo, Conta) %>%
     mutate(Saldo = ifelse(lubridate::month(Data) %in% c(6,12), Saldo[2]-Saldo[1], Saldo)) %>%
     ungroup() %>% select(-Trimestre, -Semestre, -Ano)
-}
-
-#Extra
-
-#Pega os Scores de Crédito do Santander no Outlook
-get_score_s3 = function(){
-  require(tidyverse)
-  require(Microsoft365R)
-  outl <- get_business_outlook()
-  inbox = outl$get_inbox()
-  curvas_marcacao = inbox$list_emails(search =  'Curvas Marcação - S3 Caceis', n = 999)
-  
-  curvas_marcacao = lapply(curvas_marcacao, function(x) x$list_attachments()) %>% unlist()
-  planilhas_curvas = curvas_marcacao[grep(lapply(curvas_marcacao, function(x) x$properties$name) %>% unlist(),pattern = 'Matriz')]
-  
-  df_scores_s3 = map_df(planilhas_curvas, 
-                        function(x){
-                          tp_dir = tempdir()
-                          tp_file = tempfile(tmpdir = tp_dir,fileext = '.xlsx')
-                          x$download(dest = tp_file, overwrite = T)
-                          data = x$properties$name %>% str_extract('\\d+')
-                          score_s3 = readxl::read_excel(tp_file, range = 'A1:C1000') %>% na.omit() %>% mutate(data = as.Date(data, '%d%m%Y'))
-                          colnames(score_s3) <- c('banco', 'ymf', 'rating', 'data')
-                          score_s3
-                        })
-  
-  return(df_scores_s3)
 }
